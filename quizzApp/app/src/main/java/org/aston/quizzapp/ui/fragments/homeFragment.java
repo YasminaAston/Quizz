77ca9bdea3d7f518a868fdc6e320a63fa89a844a
ABDELFADEIL <abdalfadeil@gamil.com>
2021-08-05 19:01:06 +0200

    game page

--- quizzApp/app/src/main/java/org/aston/quizzapp/ui/fragments/homeFragment.java
+++ quizzApp/app/src/main/java/org/aston/quizzapp/ui/fragments/homeFragment.java
@@ -1,5 +1,7 @@
 package org.aston.quizzapp.ui.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,6 +21,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import org.aston.quizzapp.R;
 import org.aston.quizzapp.databinding.FragmentHomeBinding;
+import org.aston.quizzapp.ui.GameActivity;
+import org.aston.quizzapp.ui.MainActivity;
 import org.aston.quizzapp.viewmodel.CategoryViewModel;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -66,6 +70,27 @@ public class homeFragment extends Fragment {
         categoryViewModel.getCategory(1);
         return inflater.inflate(R.layout.fragment_home, container, false);
 
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button =(Button)view.findViewById(R.id.btn_game_page);
+        // navigate to game page using btnGame
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Navigation.findNavController(view).navigate(R.id.gameFragment);
+                        switchActivityGame();
+                    }
+                }
+        );
+    }
+
+    private void switchActivityGame() {
+        Intent switchActivityIntent = new Intent(getActivity(), GameActivity.class);
+        startActivity(switchActivityIntent);
+        // ((Activity) getActivity()).overridePendingTransition(0, 0);
     }
 }
