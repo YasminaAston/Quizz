41f274221957388a6ce413e390271106652927b8
ABDELFADEIL <abdalfadeil@gmail.com>
2021-08-05 16:48:58 +0200

    modifs

--- quizzApp/app/src/main/java/org/aston/quizzapp/util/Constants.java
+++ quizzApp/app/src/main/java/org/aston/quizzapp/util/Constants.java
@@ -3,7 +3,7 @@ package org.aston.quizzapp.util;
 public class Constants {
 
 
-    public static final String API_URL = "http://127.0.0.1:8000/";
+    public static final String API_URL = "http://172.18.0.1:8000/";
 
     //public static final String API_URL = "http://192.168.1.19:8000/";
 
