a6adb8ea60ce347ff633fed6184eb64e2f9db771
ABDELFADEIL <abdalfadeil@gamil.com>
2021-08-06 12:45:33 +0200

    game page

--- quizzApp/app/src/main/java/org/aston/quizzapp/ui/GameActivity.java
+++ quizzApp/app/src/main/java/org/aston/quizzapp/ui/GameActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
+import androidx.cardview.widget.CardView;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,16 +24,23 @@ public class GameActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityGameBinding binding;
-
+    private CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityGameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-       // setSupportActionBar(binding.toolbar);
+        cardView = (CardView)findViewById(R.id.card_1);
+        cardView.setOnClickListener(v -> {
+           System.out.println("VieCard clicked ... ");
+        });
 
     }
+
+
+
+
 
 
 }
