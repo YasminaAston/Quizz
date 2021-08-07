8042987292562d17b4ff75bd88bcaf42191d51dd
FOUZIA HAJ <hfouzia@hotmail.fr>
2021-08-07 18:27:47 +0200

    ajout url api

--- quizzApp/app/src/main/java/org/aston/quizzapp/util/Constants.java
+++ quizzApp/app/src/main/java/org/aston/quizzapp/util/Constants.java
@@ -3,10 +3,11 @@ package org.aston.quizzapp.util;
 public class Constants {
 
 
-    public static final String API_URL = "http://172.18.0.1:8000/";
+    public static final String API_URL = "http://192.168.1.19:8000/";
 
-    //public static final String API_URL = "http://192.168.1.19:8000/";
+    //Fouzia public static final String API_URL = "http://192.168.1.19:8000/";
 
     // 172.18.0.1 // 172.18.0.1
     // 10.0.2.15 //
+    // Fouzia
 }
